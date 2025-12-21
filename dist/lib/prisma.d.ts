81ddf190c18a8437ab8600984a3d6d145ae89db2
gustavo-gsilva <prazeresdasilvag@gmail.com>
2025-12-21 15:47:46 -0300

    fix: ignore dist directory

--- dist/lib/prisma.d.ts
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-declare const prisma: import("../src/generated/prisma/internal/class.js").PrismaClient<never, import("../src/generated/prisma/internal/prismaNamespace.js").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
-export { prisma };
-//# sourceMappingURL=prisma.d.ts.map
--- dist/src/generated/prisma/models/Genre.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Genre.js.map
